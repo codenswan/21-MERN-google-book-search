2f91c09fa40f6572ee6fdf4b40ecf51f9c28b2cf
Dan <rodger.swan@gmail.com>
2020-10-12 22:07:48 +1100

    search and save routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Saved from './pages/Saved.jsx';
+import Search from './pages/Search';
+// import NoMatch from "./pages/NoMatch"
 import './App.css';
 
 export default function App() {
   return (
-    <div >
-      Start
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path={'/search'}>
+            <Search />
+          </Route>
+          <Route exact path={['/', '/saved']}>
+            <Saved />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
