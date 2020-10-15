d6f2c9c664780d1ad1105071fc1aadd7c843f1bc
Dan <rodger.swan@gmail.com>
2020-10-15 18:27:23 +1100

    change to arrow function

--- client/src/utils/googleAPI.jsx
+++ client/src/utils/googleAPI.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default {
-  searchGoogleBooks: async function (query) {
+  searchGoogleBooks: async (query) => {
     const bookResults = await axios.get(
       `/api/search?title=${query}`
     );
