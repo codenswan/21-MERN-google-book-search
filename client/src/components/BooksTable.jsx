e956da09809491ad2a461f9893911207ff599e6b
Dan <rodger.swan@gmail.com>
2020-10-15 16:20:17 +1100

    remove import of link

--- client/src/components/BooksTable.jsx
+++ client/src/components/BooksTable.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Button, Card, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 
 export default function BooksTable(props) {
   return (
