2a81f96885a92c7c4c0aa3e9efa2c648a7a2e3a1
Dan <rodger.swan@gmail.com>
2020-10-15 12:16:25 +1100

    link to google books page working

--- client/src/components/BooksTable.jsx
+++ client/src/components/BooksTable.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Card, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export default function BooksTable(props) {
   return (
@@ -15,7 +16,12 @@ export default function BooksTable(props) {
             </Card.Title>
             <Card.Text> Author: {book.Author}</Card.Text>
             <Card.Text> Desciption: {book.Description} </Card.Text>
-            <Button onClick={() => props.handleSaveBtn(book)} variant="primary">Save</Button>
+            <Button variant="primary" className="mr-2" onClick={() => props.handleSaveBtn(book)}>
+              Save
+            </Button>
+            <Button variant="link" target="_blank" href={book.Link}>
+             View Details 
+            </Button>
           </Card.Body>
         </Card>
       ))}
