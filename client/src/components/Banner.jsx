d8397a33f63f6cb7a264aac55395ac52bb5bafe7
Dan <rodger.swan@gmail.com>
2020-10-19 10:00:43 +1100

    add span for title

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -5,6 +5,7 @@ export default function Banner() {
 
     return(
         <Navbar bg="dark" variant="dark">
+            <span className="navbar-brand mb-0 h1">Google Book Search</span>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav.Link href="/search">Search</Nav.Link>
                 <Nav.Link href="/saved">Saved</Nav.Link>
