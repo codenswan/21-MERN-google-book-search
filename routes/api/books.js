31aa6c4b1e969e9a8f977f46e117d1b058a64644
Dan <rodger.swan@gmail.com>
2020-10-18 19:27:05 +1100

    delete route

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,10 +1,12 @@
-const router = require("express").Router();
-const booksController = require("../../controllers/booksController");
+const router = require('express').Router();
+const booksController = require('../../controllers/booksController');
 
 // Matches with "/api/books"
-router.route("/")
+router.route('/')
   .get(booksController.findAll)
   .post(booksController.create);
 
+router.route('/:id')
+  .delete(booksController.remove);
 
 module.exports = router;
