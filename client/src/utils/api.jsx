6a32ed3ef7dcc796a056dc6ca8f67f85f39d8850
Dan <rodger.swan@gmail.com>
2020-10-18 19:27:20 +1100

    removed get book by id

--- client/src/utils/api.jsx
+++ client/src/utils/api.jsx
@@ -2,15 +2,11 @@ import axios from 'axios';
 
 export default {
   //* Fetches books from database
-  getBooks: () =>{
+  getBooks: () => {
     return axios.get('/api/books');
   },
-  //* Gets the book with the given id
-  getBook: (id) =>{
-    return axios.get('/api/books/' + id);
-  },
   //* Deletes the book with the given id
-  deleteBook: (id) =>{
+  deleteBook: (id) => {
     return axios.delete('/api/books/' + id);
   },
   //* Saves a book to the database
